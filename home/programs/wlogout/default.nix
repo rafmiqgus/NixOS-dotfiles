5f5c0cf2b7ed062ea3a7be772fee755808775924
Rafael Miqueles Gustafsson <rafaelmiqgus@gmail.com>
2025-08-23 23:41:30 +0200

    added wlogout

---
+++ home/programs/wlogout/default.nix
@@ -0,0 +1,40 @@
+{ config, inputs, lib, ... }:
+
+{
+  programs.wlogout = {
+    enable = true;
+    layout = [
+      {
+        "label" = "shutdown";
+        "action" = "systemctl poweroff";
+        "text" = "Shutdown";
+        "keybind" = "s";
+      }
+      {
+        "label" = "reboot";
+        "action" = "systemctl reboot";
+        "text" = "Reboot";
+        "keybind" = "r";
+      }
+      {
+        "label" = "logout";
+        "action" = "loginctl kill-session $XDG_SESSION_ID";
+        "text" = "Logout";
+        "keybind" = "e";
+      }
+      {
+        "label" = "sleep";
+        "action" = "loginctl lock-session && systemctl suspend";
+        "text" = "Sleep";
+        "keybind" = "h";
+      }
+      {
+        "label" = "lock";
+        "action" = "loginctl lock-session";
+        "text" = "Lock";
+        "keybind" = "l";
+      }
+    ];
+    style = ./wlogout.css;
+  };
+}
